667b3bc083045525cf736d120a7304062dcba422
firdiaudi <audifirdi@gmail.com>
2025-03-11 23:01:27 +0700

    fix#1.3

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,6 +3,10 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { prisma } from "./prisma";
 
+export const config = {
+  runtime: 'nodejs'
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
