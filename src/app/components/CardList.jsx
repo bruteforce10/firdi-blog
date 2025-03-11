b326476d7f5b0e2d33cb76b52894c903499d7157
firdiaudi <audifirdi@gmail.com>
2025-03-11 22:52:18 +0700

    fix#1.1

--- src/app/components/CardList.jsx
+++ src/app/components/CardList.jsx
@@ -26,7 +26,7 @@ const CardList = async ({ page, cat }) => {
     <div className="flex-[5]">
       <h1 className="text-3xl font-bold my-4">Recent Posts</h1>
       <div className="flex flex-col">
-        {posts?.map((item) => (
+        {posts && posts?.map((item) => (
           <Card item={item} key={item._id} />
         ))}
       </div>
