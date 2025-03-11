c5b8aead4b8f605e92fc01e58752268394f87b71
firdiaudi <audifirdi@gmail.com>
2025-03-12 05:17:25 +0700

    bismillah fix

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -2,9 +2,7 @@
 import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
-export const config = {
-  runtime: 'nodejs',
-};
+export const runtime = "nodejs";
 
 
 export const GET = async () => {
--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -1,9 +1,7 @@
 import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
-export const config = {
-  runtime: 'nodejs',
-};
+export const runtime = "nodejs";
 
 
 export const GET = async (req, { params }) => {
