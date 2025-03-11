5908b3380227aec84a1ee121946b3c8c8e5390ea
firdiaudi <audifirdi@gmail.com>
2025-03-12 05:24:39 +0700

    Tambahkan Konfigurasi Paket Eksternal untuk Komponen Server Next.js
    - Konfigurasikan paket @prisma/client dan @auth/prisma-adapter untuk komponen server
    - Gunakan properti experimental.serverComponentsExternalPackages di next.config.mjs
    - Pastikan kompatibilitas dengan konfigurasi server Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', '@auth/prisma-adapter'],
+  },
 };
 
 export default nextConfig;
